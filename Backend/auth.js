b49003205774e3727d8cb4dc58223a5d9e22d134
brauliodeus <braulio.palma05@gmail.com>
2025-11-22 17:15:13 -0300

    token expiration updated

--- Backend/auth.js
+++ Backend/auth.js
@@ -71,7 +71,7 @@ router.post('/login', async (req, res) => {
     jwt.sign(
       payload, 
       process.env.JWT_SECRET, 
-      { expiresIn: '1h' }, 
+      { expiresIn: '30m' }, 
       (err, token) => {
         if (err) throw err;
         res.json({ 
